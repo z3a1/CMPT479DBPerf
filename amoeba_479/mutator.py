6c691aacbe122502ef11209851c2942941889256
Ameer Nur Saidy <ansnursaidy@gmail.com>
2025-08-11 05:09:18 -0700

    preprocess completed

--- amoeba_479/mutator.py
+++ amoeba_479/mutator.py
@@ -1,34 +1,15 @@
-from mutatation_rules import MUTATE_RULES
-# from sqlglot import parse_one, exp
 import jpype
 import jpype.imports
 from jpype.types import *
 import os
-
-
+from dotenv import load_dotenv
 
 
 java_lib_dir = "target/dependency"
 dep_dir = "target/dependency"
 
-# jars = [
-#     "calcite-core-1.40.0.jar",
-#     "calcite-linq4j-1.40.0.jar",
-#     "guava-33.3.0-jre.jar",
-#     "jackson-annotations-2.15.4.jar",
-#     "jackson-core-2.15.4.jar",
-#     "jackson-databind-2.15.4.jar",
-#     "slf4j-api-1.7.25.jar",
-#     "commons-codec-1.16.0.jar",
-#     "commons-io-2.15.0.jar",
-#     "avatica-core-1.26.0.jar",
-#     "slf4j-simple-1.7.25.jar",
-#     "jts-core-1.20.0.jar"
-# ]
 jars = [os.path.join(dep_dir, jar) for jar in os.listdir(dep_dir) if jar.endswith(".jar")]
-# classpath = os.pathsep.join(os.path.join(java_lib_dir, jar) for jar in jars)
 classpath = os.pathsep.join(jars)
-
 jvm_path = jpype.getDefaultJVMPath()
 
 jpype.startJVM(jvm_path,f"-Djava.class.path={classpath}")
@@ -40,13 +21,13 @@ from org.apache.calcite.tools import Frameworks
 from org.apache.calcite.rel.rel2sql import RelToSqlConverter
 from org.apache.calcite.rel.rules import CoreRules
 from org.apache.calcite.schema import SchemaPlus
-from org.apache.calcite.jdbc import CalciteSchema
-from org.apache.calcite.schema.impl import AbstractTable
+from org.apache.calcite.sql.dialect import PostgresqlSqlDialect 
 from org.apache.calcite.rel.type import RelDataTypeFactory
 from org.apache.calcite.sql.type import SqlTypeName
-
-
-
+from org.apache.calcite.adapter.jdbc import JdbcSchema
+from org.apache.calcite.sql import SqlDialect
+from java.sql import DriverManager
+from java.util import Properties
 
 
 
@@ -71,69 +52,127 @@ def rules_initialization():
     ]
 
 
+
 #preprocess base query and generates logical query plan tree r_origin (apply mutation rules on this)
 def preprocess(base_query):
+    load_dotenv()
 
-    pass
+    db_url = os.getenv("DATABASE_URL")
+    
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    host = os.getenv("HOST")
+    port = 5432
+    db_name = os.getenv("DATABASE_NAME")
+
+    # connectionProps = Properties()
+    # connectionProps.setProperty("user", user)
+    # connectionProps.setProperty("password", password)
     
 
-def apply_rule(target_expr, rule):
-    pass
-
-
-def translate_to_query(r_new):
-    if r_new == None:
-        return None
+    jdbc_url = f"jdbc:postgresql://{host}:{port}/{db_name}"
+  
+    # connection = DriverManager.getConnection(jdbc_url, connectionProps)
+    ds = JdbcSchema.dataSource(
+        jdbc_url,
+        "org.postgresql.Driver",
+        user,
+        password
+    )
+    root_schema = Frameworks.createRootSchema(True)
    
+    jdbc_schema = JdbcSchema.create(
+        root_schema,
+        "public",
+        ds,
+        None,        
+        "public"     
+    )
+    root_schema.add("public", jdbc_schema)
+
+    parserConfig = SqlParser.config().withCaseSensitive(False) 
+    config = Frameworks.newConfigBuilder().parserConfig(parserConfig).defaultSchema(root_schema.getSubSchema("public")).build()
+
+    planner = Frameworks.getPlanner(config)
+
+    sql_node = planner.parse(base_query)
+    # print(sql_node)
+    validate_node = planner.validate(sql_node)
+    # print(validate_node)
+    rel_root = planner.rel(validate_node)
+    
+  
+    print(rel_root) 
+    
+    # Use this for rule mutations
+    return rel_root 
+
+
+
+# def apply_rule(target_expr, rule):
+
+
+
+# def translate_to_query(r_new):
+#     if r_new == None:
+#         return None
+#     else:
+#         converter = RelToSqlConverter(None)
+#         sql_node = converter.visitRoot(r_new).asStatement()
+#         return sql_node.toString()
     
 
-def update(transformed_trees, mutant_queries,  r_new, new_query):
-    # MUTATOR translates Rnew into a well-formed
-    # SQL query, new query, based on the target DBMS’s dialect and
-    # appends it to mutant queries
+# def update(transformed_trees, mutant_queries,  r_new, new_query):
+#     # MUTATOR translates Rnew into a well-formed
+#     # SQL query, new query, based on the target DBMS’s dialect and
+#     # appends it to mutant queries
 
-    transformed_trees.append(r_new)
-    mutant_queries.append(new_query)
+#     transformed_trees.append(r_new)
+#     mutant_queries.append(new_query)
 
 
-#Transforms r_origin using mutate_rules
-def mutate_tree(r_origin, mutate_rules):
-    target_expr = r_origin
-    for rule in mutate_rules:
-        target_expr = apply_rule(target_expr, rule)
-    if target_expr != r_origin:
-        return target_expr
+# #Transforms r_origin using mutate_rules
+# def mutate_tree(r_origin, mutate_rules):
+#     target_expr = r_origin
+#     for rule in mutate_rules:
+#         target_expr = apply_rule(target_expr, rule)
+#     if target_expr != r_origin:
+#         return target_expr
     
 
 
-#base_query and meta data of target database
-def mutate_query(base_query):
-    number_of_attempts = 10
-    mutant_queries = []
-    transformed_trees= []
-    r_origin = preprocess(base_query)
+# #base_query and meta data of target database
+# def mutate_query(base_query):
+#     number_of_attempts = 10
+#     mutant_queries = []
+#     transformed_trees= []
+#     r_origin = preprocess(base_query)
 
-    for k in range(number_of_attempts):
-        mutate_rules = rules_initialization()
-        r_new = mutate_tree(r_origin, mutate_rules)
-        if r_new not in transformed_trees:
-            new_query = translate_to_query(r_new)
-            if new_query is not None:
-                update(transformed_trees, mutant_queries, r_new, new_query)
+#     for k in range(number_of_attempts):
+#         mutate_rules = rules_initialization()
+#         r_new = mutate_tree(r_origin, mutate_rules)
+#         if r_new not in transformed_trees:
+#             new_query = translate_to_query(r_new)
+#             if new_query is not None:
+#                 update(transformed_trees, mutant_queries, r_new, new_query)
 
-    return base_query, mutant_queries
+#     return base_query, mutant_queries
+
+
 
 
 
 
 def main():
-    base_query = "SELECT col FROM (SELECT * FROM my_table) AS sub WHERE col > 5 GROUP BY col"
-    mutant_queries = []
+    preprocess("SELECT * FROM pokemon INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id")  
+
+    # base_query = "SELECT * FROM pokemon INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id"
+    # mutant_queries = []
    
-    base_query, mutant_queries = mutate_query(base_query)
+    # base_query, mutant_queries = mutate_query(base_query)
     
-    for query in mutant_queries:
-        print(query)
+    # for query in mutant_queries:
+    #     print(query)
 
     jpype.shutdownJVM()
 if __name__ == "__main__":
