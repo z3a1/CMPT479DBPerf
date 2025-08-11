847d6669338e01b16aa724ea440b62dd988c4d54
Ameer Nur Saidy <ansnursaidy@gmail.com>
2025-08-11 14:52:55 -0700

    working apply_rule

--- amoeba_479/mutator.py
+++ amoeba_479/mutator.py
@@ -33,22 +33,23 @@ from java.util import Properties
 
 def rules_initialization():
     return [
-        CoreRules.AGGREGATE_CASE_TO_FILTER,
-        CoreRules.AGGREGATE_EXPAND_DISTINCT_AGGREGATES,
-        CoreRules.AGGREGATE_FILTER_TRANSPOSE,
-        CoreRules.AGGREGATE_JOIN_TRANSPOSE,
-        CoreRules.AGGREGATE_REMOVE,
-        CoreRules.FILTER_AGGREGATE_TRANSPOSE,
-        CoreRules.FILTER_INTO_JOIN,
-        CoreRules.FILTER_MERGE,
-        CoreRules.FILTER_PROJECT_TRANSPOSE,
-        CoreRules.JOIN_PUSH_EXPRESSIONS,
-        CoreRules.PROJECT_JOIN_TRANSPOSE,
-        CoreRules.PROJECT_MERGE,
-        CoreRules.PROJECT_REMOVE,
+        CoreRules.AGGREGATE_CASE_TO_FILTER, #WORKS
+        CoreRules.AGGREGATE_EXPAND_DISTINCT_AGGREGATES, #WORKS
+        CoreRules.AGGREGATE_FILTER_TRANSPOSE, #WORKS
+        CoreRules.AGGREGATE_JOIN_TRANSPOSE, #WORKS
+        CoreRules.AGGREGATE_REMOVE, #WORKS
+        CoreRules.FILTER_AGGREGATE_TRANSPOSE, #WORKS
+        CoreRules.FILTER_INTO_JOIN, #WORKS
+        CoreRules.FILTER_MERGE, #WORKS
+        CoreRules.FILTER_PROJECT_TRANSPOSE, #WORKS
+        CoreRules.JOIN_PUSH_EXPRESSIONS, #WORKS
+        CoreRules.PROJECT_JOIN_TRANSPOSE, #WORKS
+        CoreRules.PROJECT_MERGE, #WORKS
+        CoreRules.PROJECT_REMOVE, #WORKS
         CoreRules.SORT_REMOVE,
         CoreRules.SORT_PROJECT_TRANSPOSE,
         CoreRules.UNION_TO_DISTINCT
+      
     ]
 
 
@@ -65,14 +66,8 @@ def preprocess(base_query):
     port = 5432
     db_name = os.getenv("DATABASE_NAME")
 
-    # connectionProps = Properties()
-    # connectionProps.setProperty("user", user)
-    # connectionProps.setProperty("password", password)
-    
-
     jdbc_url = f"jdbc:postgresql://{host}:{port}/{db_name}"
-  
-    # connection = DriverManager.getConnection(jdbc_url, connectionProps)
+
     ds = JdbcSchema.dataSource(
         jdbc_url,
         "org.postgresql.Driver",
@@ -108,60 +103,60 @@ def preprocess(base_query):
 
 
 
-def apply_rule(rule):
-    target_expr = preprocess("SELECT * FROM pokemon INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id")
-    program = HepProgram.builder().addRuleInstance(CoreRules.FILTER_INTO_JOIN).build()
+def apply_rule(target_expr, rule):
+    program = HepProgram.builder().addRuleInstance(rule).build()
     hepPlanner = HepPlanner(program)
     hepPlanner.setRoot(target_expr)
-    hepPlanner.findBestExp().explain()
-    print(hepPlanner)
-apply_rule(CoreRules.FILTER_INTO_JOIN)
+    return hepPlanner.findBestExp()
+    # print(hepPlanner)
 
-# def translate_to_query(r_new):
-#     if r_new == None:
-#         return None
-#     else:
-#         converter = RelToSqlConverter(None)
-#         sql_node = converter.visitRoot(r_new).asStatement()
-#         return sql_node.toString()
+
+def translate_to_query(r_new):
+    if r_new == None:
+        return None
+    else:
+        converter = RelToSqlConverter(None)
+        sql_node = converter.visitRoot(r_new).asStatement()
+        return sql_node.toString()
     
 
-# def update(transformed_trees, mutant_queries,  r_new, new_query):
-#     # MUTATOR translates Rnew into a well-formed
-#     # SQL query, new query, based on the target DBMS’s dialect and
-#     # appends it to mutant queries
+def update(transformed_trees, mutant_queries,  r_new, new_query):
+    # MUTATOR translates Rnew into a well-formed
+    # SQL query, new query, based on the target DBMS’s dialect and
+    # appends it to mutant queries
 
-#     transformed_trees.append(r_new)
-#     mutant_queries.append(new_query)
+    transformed_trees.append(r_new)
+    mutant_queries.append(new_query)
 
 
-# #Transforms r_origin using mutate_rules
-# def mutate_tree(r_origin, mutate_rules):
-#     target_expr = r_origin
-#     for rule in mutate_rules:
-#         target_expr = apply_rule(target_expr, rule)
-#     if target_expr != r_origin:
-#         return target_expr
+#Transforms r_origin using mutate_rules
+def mutate_tree(r_origin, mutate_rules):
+    target_expr = r_origin
+    for rule in mutate_rules:
+        print(rule)
+        target_expr = apply_rule(target_expr, rule)
+    if target_expr != r_origin:
+        return target_expr
     
 
 
-# #base_query and meta data of target database
-# def mutate_query(base_query):
-#     number_of_attempts = 10
-#     mutant_queries = []
-#     transformed_trees= []
-#     r_origin = preprocess(base_query)
-#     print("R_ORIGIN =", r_origin)
+#base_query and meta data of target database
+def mutate_query(base_query):
+    number_of_attempts = 10
+    mutant_queries = []
+    transformed_trees= []
+    r_origin = preprocess(base_query)
+    print("R_ORIGIN =", r_origin)
 
-#     for k in range(number_of_attempts):
-#         mutate_rules = rules_initialization()
-#         r_new = mutate_tree(r_origin, mutate_rules)
-#         if r_new not in transformed_trees:
-#             new_query = translate_to_query(r_new)
-#             if new_query is not None:
-#                 update(transformed_trees, mutant_queries, r_new, new_query)
+    for k in range(number_of_attempts):
+        mutate_rules = rules_initialization()
+        r_new = mutate_tree(r_origin, mutate_rules)
+        if r_new not in transformed_trees:
+            new_query = translate_to_query(r_new)
+            if new_query is not None:
+                update(transformed_trees, mutant_queries, r_new, new_query)
 
-#     return base_query, mutant_queries
+    return base_query, mutant_queries
 
 
 
@@ -171,13 +166,13 @@ apply_rule(CoreRules.FILTER_INTO_JOIN)
 def main():
     # preprocess("SELECT * FROM pokemon INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id")  
 
-    # base_query = "SELECT * FROM pokemon INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id"
-    # mutant_queries = []
+    base_query = "SELECT * FROM pokemon INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id"
+    mutant_queries = []
    
-    # base_query, mutant_queries = mutate_query(base_query)
+    base_query, mutant_queries = mutate_query(base_query)
     
-    # for query in mutant_queries:
-    #     print(query)
+    for query in mutant_queries:
+        print(query)
 
     jpype.shutdownJVM()
 if __name__ == "__main__":
